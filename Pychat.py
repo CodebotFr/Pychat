623f1cbf6b2ebe386520f5e53e0534d23bb6cbdb
Alexis-mjk <codebot@outlook.fr>
2018-03-30 17:59:12 +0200

    config initialisation

--- Pychat.py
+++ Pychat.py
@@ -4,20 +4,15 @@
 #Author: Codebot
 
 #import module
-import sys
-import os
 from threading import Thread
 import socket
-import xml.etree.ElementTree as ET
-#import lxml
+import configparser
+import os
 
-#import local files
-os.chdir(os.getcwd() + "/data/")
-tree = ET.parse('settings.xml')
-root = tree.getroot()
-for username in root.iter("username"):
-	if username.text is None :
-		nusername = input("username :")
-		username.text = nusername
-	else:
-		print(username.text)
+#import config
+os.getcwd()
+os.chdir("./data")
+config = configparser.ConfigParser()
+config.read("config.ini")
+default = config['DEFAULT']['FIRST_USE']
+
